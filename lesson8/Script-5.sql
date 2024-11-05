ab8bd0664fc72c3c002e1d942c1c8e952c8391c6
andylai888 <lfwlds@gmail.com>
2024-11-05 15:22:07 +0800

    modify

--- lesson8/Script-5.sql
+++ lesson8/Script-5.sql
@@ -3,7 +3,6 @@ INSERT INTO records
 values
     ('屏東(枋山)', '屏東縣', 34, '良好', 18, '2024-11-04 10:00', 22.260899, 120.651472);
 
-INSERT OR
-IGNORE INTO records(sitename,county,aqi,status,pm25,date,lat,lon)
+INSERT OR IGNORE INTO records(sitename,county,aqi,status,pm25,date,lat,lon)
 values
     ('屏東(枋山)', '屏東縣', 34, '良好', 18, '2024-11-04 10:00', 22.260899, 120.651472);
