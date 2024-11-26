daf9a4fb2ec0c3a6d31e3de042443be280fac922
andylai888 <lfwlds@gmail.com>
2024-11-26 17:32:14 +0800

    modify

--- BMI計算器/tools.py
+++ BMI計算器/tools.py
@@ -19,33 +19,33 @@ class CustomMessagebox(Dialog):
         contain_frame = ttk.Frame(master,style='Input.TFrame')
       
         label_name = ttk.Label(contain_frame, text="姓名:")
-        label_name.grid(row=0, column=0, padx=5, pady=5,sticky=tk.E)
+        label_name.grid(row=0, column=0, padx=10, pady=10,sticky=tk.E)
 
         self.value_name = ttk.Label(contain_frame,text=self.name)
-        self.value_name.grid(row=0, column=1, padx=5, pady=5)
+        self.value_name.grid(row=0, column=1, padx=10, pady=10)
 
        
         label_bmi = ttk.Label(contain_frame, text="BMI值:")
-        label_bmi.grid(row=1, column=0, padx=5, pady=5,sticky=tk.E)
+        label_bmi.grid(row=1, column=0, padx=10, pady=10,sticky=tk.E)
 
         self.value_height = ttk.Label(contain_frame,text=f'{self.bmi:.2f}')
-        self.value_height.grid(row=1, column=1, padx=5, pady=5)
+        self.value_height.grid(row=1, column=1, padx=10, pady=10)
 
         
         label_status = ttk.Label(contain_frame, text="狀態:")
-        label_status.grid(row=2, column=0, padx=5, pady=5,sticky=tk.E)
+        label_status.grid(row=2, column=0, padx=10, pady=10,sticky=tk.E)
 
         self.value_status = ttk.Label(contain_frame,text=self.status,style='status.TLabel')
-        self.value_status.grid(row=2, column=1, padx=5, pady=5)
+        self.value_status.grid(row=2, column=1, padx=10, pady=10)
 
        
         label_advice = ttk.Label(contain_frame, text="建議:")
-        label_advice.grid(row=3, column=0, padx=5, pady=5,sticky=tk.E)
+        label_advice.grid(row=3, column=0, padx=10, pady=10,sticky=tk.E)
 
         self.value_advice = ttk.Label(contain_frame,text=self.advice)
-        self.value_advice.grid(row=3, column=1, padx=5, pady=5)   
+        self.value_advice.grid(row=3, column=1, padx=10, pady=10)   
 
-        contain_frame.pack(pady=10,padx=30)
+        contain_frame.pack(pady=50,padx=100)
 
     def apply(self):
         
@@ -57,7 +57,7 @@ class CustomMessagebox(Dialog):
        
         box = ttk.Frame(self)
         self.ok_button = tk.Button(box, text="確定", width=10, command=self.ok, default=tk.ACTIVE)
-        self.ok_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.ok_button.pack(side=tk.LEFT, padx=10, pady=10)
         box.pack()
 
     def ok(self):
