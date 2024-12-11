e8909cb768e047d41732e65f11abd1534639c66a
andylai888 <lfwlds@gmail.com>
2024-12-11 17:18:54 +0800

    modify

--- AI專題/readme_proj/view/__init__.py
+++ AI專題/readme_proj/view/__init__.py
@@ -1,3 +1,3 @@
 from .sitename_frame import SitenameFrame
-from .image_butto import ImageButton
-from .item_dialig import MyCustomDialog
+from .image_button import ImageButton
+from .item_dialog import MyCustomDialog
