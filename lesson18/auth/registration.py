ccf48f089a84972321518a89d837f519e803af01
andylai888 <lfwlds@gmail.com>
2024-12-17 17:42:51 +0800

    新增2024_12_17分支

--- lesson18/auth/registration.py
+++ lesson18/auth/registration.py
@@ -3,6 +3,7 @@ from flask import render_template,request,redirect,url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,EmailField,validators
 from werkzeug.security import generate_password_hash
+from datasource import is_email_duplicate,add_user
 
 
 class RegistrationForm(FlaskForm):
@@ -31,6 +32,13 @@ def regist():
         password = form.password.data
         password_hash = generate_password_hash(password=password,method='pbkdf2:sha256',salt_length=8)
         print(password_hash)        
-        print("驗証通過")       
-        return redirect(url_for('auth.success'))
+        if not is_email_duplicate(email):
+            #email沒有重覆
+            if add_user(name,email,password_hash):
+                return redirect(url_for('auth.success'))
+            else:
+                print("加入失敗")
+        else:
+            print('email有重覆')
+        
     return render_template('auth/registration.j2',form=form)
